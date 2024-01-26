fe90139fa02c5e4f1c106215c0e1c0f276952e80
harupy <17039389+harupy@users.noreply.github.com>
2024-01-26 13:16:38 +0900

    hide

--- website/src/posts.ts
+++ website/src/posts.ts
@@ -37,21 +37,6 @@ export const latestBlogs: Blog[] = [
     thumbnail: "img/blog/2023-year-in-review.png",
   },
   {
-    title: "Streamline your MLflow Projects with Free Hosted MLflow",
-    path: "blog/2024/01/25/databricks-ce",
-    tags: ["managed mlflow", "getting started"],
-    authors: [
-      {
-        name: "Abe Omorogbe",
-        title: "Product Manager, ML at Databricks",
-        url: "https://www.linkedin.com/in/abeomor/",
-        image_url:
-          "https://media.licdn.com/dms/image/D4E03AQG-dAkLO2hx_Q/profile-displayphoto-shrink_400_400/0/1693252025753?e=1711584000&v=beta&t=02Bl1a73vp7Zpt8ZXSK_BRDF6DIZWAx1l8oSgkWNa5g",
-      },
-    ],
-    date: "2024-01-25",
-  },
-  {
     title: "MLflow AI Gateway renamed to MLflow Deployments for LLMs",
     path: "blog/2023/12/01/ai-gateway-rename",
     tags: ["ai"],
@@ -65,6 +50,22 @@ export const latestBlogs: Blog[] = [
     ],
     date: "2023-12-01",
     thumbnail: "img/blog/ai-gateway.png",
+  },
+  {
+    title:
+      "Automatic Metric, Parameter, and Artifact Logging with mlflow.autolog",
+    path: "blog/2023/11/30/mlflow-autolog",
+    tags: ["autolog"],
+    authors: [
+      {
+        name: "Daniel Liden",
+        title: "Developer Advocate at Databricks",
+        url: "https://www.linkedin.com/in/danielliden",
+        image_url: "https://github.com/djliden.png",
+      },
+    ],
+    date: "2023-11-30",
+    thumbnail: "img/blog/mlflow-autolog.png",
   },
 ];
 
