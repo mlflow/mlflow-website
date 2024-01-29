7af3797ffdc41704e347d41a346486e37135306a
Harutaka Kawamura <hkawamura0130@gmail.com>
2024-01-29 14:58:53 +0900

    Remove tutorials (#17)

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -33,13 +33,7 @@ const config: Config = {
     [
       "classic",
       {
-        docs: {
-          sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
+        docs: false,
         blog: {
           showReadingTime: true,
           blogSidebarTitle: "All posts",
