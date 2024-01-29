5984689c57e161d3233763cb6e1ae9c9826d6bb3
harupy <17039389+harupy@users.noreply.github.com>
2024-01-29 14:53:03 +0900

    Disable docs

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -33,13 +33,14 @@ const config: Config = {
     [
       "classic",
       {
-        docs: {
-          sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
+        docs: false,
+        // docs: {
+        //   sidebarPath: "./sidebars.ts",
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   // editUrl:
+        //   //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        // },
         blog: {
           showReadingTime: true,
           blogSidebarTitle: "All posts",
