ba90da5fe88dde2c1b0db4057bc7849fe4d46488
Daniel Lok <daniel.lok@databricks.com>
2024-02-28 15:13:56 +0900

    generate linnks
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- .github/workflows/format-release-note.js
+++ .github/workflows/format-release-note.js
@@ -1,10 +1,40 @@
 const formatMarkdown = ({ core, filenames }) => {
   const fs = require('fs');
-  filenames.map(filename => {
-    core.info(`file: ${filename}`);
-    const content = fs.readFileSync(filename, 'utf8');
-    core.info(`file content: ${content}`);
-  });
+
+  filenames
+    .filter(filename => filename.endsWith("release.md"))
+    .forEach(filename => {
+      const content = fs.readFileSync(filename, 'utf8');
+      const formatted = generateLinks(content);
+      core.info(formatted);
+      fs.writeFileSync(filename, formatted, 'utf8');
+    });
+}
+
+/**
+ * Generate links for PR numbers and GitHub
+ * usernames in the release note markdown content.
+ */
+const generateLinks = (content) => {
+  // [^\[] is for the case where the PR 
+  // number is already in a markdown link.
+  // this prevents errors when running the
+  // workflow on every commit.
+  const prNumberRegex = /(^|[^\[])#(\d+)/g;
+  const usernameRegex = /(^|[^\[])@([\w\d-]+)/g;
+
+  // Replace PR numbers with markdown-styled links
+  content = content.replace(
+    prNumberRegex,
+    "$1[#$2](https://www.github.com/mlflow/mlflow/pull/$2)"
+  );
+
+  content = content.replace(
+    usernameRegex,
+    "$1[@$2](https://www.github.com/$2)"
+  );
+
+  return content
 }
 
 module.exports = {
