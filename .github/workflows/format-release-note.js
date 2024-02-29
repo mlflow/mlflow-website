0102f0fc6823e8b8ef34d57b93990919006af80f
Daniel Lok <daniel.lok@databricks.com>
2024-02-29 16:31:55 +0900

    Add a workflow to automatically generate links for release notes (#31)
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>
    Co-authored-by: Harutaka Kawamura <hkawamura0130@gmail.com>

---
+++ .github/workflows/format-release-note.js
@@ -0,0 +1,41 @@
+const formatMarkdown = ({ filenames }) => {
+  const fs = require('fs');
+
+  filenames
+    .filter(filename => filename.endsWith("release.md"))
+    .forEach(filename => {
+      const content = fs.readFileSync(filename, 'utf8');
+      const formatted = generateLinks(content);
+      fs.writeFileSync(filename, formatted, 'utf8');
+    });
+}
+
+/**
+ * Generate links for PR numbers and GitHub
+ * usernames in the release note markdown content.
+ */
+const generateLinks = (content) => {
+  // [^\[] is for the case where the PR 
+  // number is already in a markdown link.
+  // this prevents errors when running the
+  // workflow on every commit.
+  const prNumberRegex = /(^|[^\[])#(\d+)/g;
+  const usernameRegex = /(^|[^\[])@([\w\d-]+)/g;
+
+  // Replace PR numbers with markdown-styled links
+  content = content.replace(
+    prNumberRegex,
+    "$1[#$2](https://github.com/mlflow/mlflow/pull/$2)"
+  );
+
+  content = content.replace(
+    usernameRegex,
+    "$1[@$2](https://github.com/$2)"
+  );
+
+  return content
+}
+
+module.exports = {
+  formatMarkdown
+}
---
+++ .github/workflows/is-mlflow-maintainer.js
@@ -0,0 +1,17 @@
+const MLFLOW_CORE_MAINTAINERS = [
+  "B-Step62",
+  "BenWilson2",
+  "daniellok-db",
+  "harupy",
+  "mlflow-automation",
+  "serena-ruan",
+  "WeichenXu123",
+]
+
+const isMLflowMaintainer = (username) => {
+    return MLFLOW_CORE_MAINTAINERS.includes(username);
+}
+
+module.exports = {
+    isMLflowMaintainer,
+}
