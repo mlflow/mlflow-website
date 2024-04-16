47d4e84fe5be41daf1065cb16c1fb2c48c60e9b2
Ben Wilson <benjamin.wilson@databricks.com>
2024-04-16 19:52:33 -0400

    generate posts
    Signed-off-by: Ben Wilson <benjamin.wilson@databricks.com>

--- website/src/posts.ts
+++ website/src/posts.ts
@@ -160,6 +160,20 @@ export const BLOGS: Blog[] = [
 // Sort by date descending
 export const RELEASES: Release[] = [
   {
+    title: "MLflow 2.12.0",
+    path: "/releases/2.12.0",
+    authors: [
+      {
+        name: "MLflow maintainers",
+        title: "MLflow maintainers",
+        url: "https://github.com/mlflow/mlflow.git",
+        image_url: "https://github.com/mlflow-automation.png",
+      },
+    ],
+    date: "2024-04-16",
+    version: "2.12.0",
+  },
+  {
     title: "MLflow 2.11.3",
     path: "/releases/2.11.3",
     authors: [
