89f8fc31637b2cade39095852df8998d8e625656
harupy <harupy@users.noreply.github.com>
2024-04-18 06:41:07 +0000

    deploy: 8ecc8e436ff139c30031ad068c411f66ee24e73b

--- docs/latest/_static/documentation_options.js
+++ docs/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.11.4.dev0',
+    VERSION: '2.12.2.dev0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
