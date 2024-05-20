e7836bd7055b7c779851e8e3e18cac77ba55ebc1
mlflow-automation <61449322+mlflow-automation@users.noreply.github.com>
2024-05-21 08:24:55 +0900

    Add release post for 2.13.0 (#63)
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>
    Signed-off-by: Ben Wilson <benjamin.wilson@databricks.com>
    Co-authored-by: mlflow-automation <mlflow-automation@users.noreply.github.com>
    Co-authored-by: Ben Wilson <benjamin.wilson@databricks.com>

--- website/src/posts.ts
+++ website/src/posts.ts
@@ -208,6 +208,20 @@ export const BLOGS: Blog[] = [
 // Sort by date descending
 export const RELEASES: Release[] = [
   {
+    title: "MLflow 2.13.0",
+    path: "/releases/2.13.0",
+    authors: [
+      {
+        name: "MLflow maintainers",
+        title: "MLflow maintainers",
+        url: "https://github.com/mlflow/mlflow.git",
+        image_url: "https://github.com/mlflow-automation.png",
+      },
+    ],
+    date: "2024-05-20",
+    version: "2.13.0",
+  },
+  {
     title: "MLflow 2.12.2",
     path: "/releases/2.12.2",
     authors: [
