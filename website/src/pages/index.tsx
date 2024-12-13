d63299f7382ff96aeedf9dc20b4d37ca58641271
B-Step62 <yuki.watanabe@databricks.com>
2024-12-13 09:37:09 +0900

    ssr
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -231,15 +231,6 @@ export default function Home(): JSX.Element {
     return () => observer.disconnect();
   }, []);
 
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => setWindowWidth(window.innerWidth);
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
     <Layout
       title={siteConfig.title}
@@ -536,144 +527,150 @@ export default function Home(): JSX.Element {
           </h2>
         </div>
 
-        <ExpandableGrid
-          items={[
-            {
-              title: "PyTorch",
-              src: "img/pytorch.svg",
-              href: "docs/latest/deep-learning/pytorch/index.html",
-            },
-            {
-              title: "HuggingFace",
-              src: "img/huggingface.svg",
-              href: "docs/latest/llms/transformers/index.html",
-            },
-            {
-              title: "OpenAI",
-              src: "img/openai.svg",
-              href: "docs/latest/llms/openai/index.html",
-            },
-            {
-              title: "LangChain",
-              src: "img/langchain.svg",
-              href: "docs/latest/llms/langchain/index.html",
-            },
-            {
-              title: "LlamaIndex",
-              src: "img/llamaindex.svg",
-              href: "docs/latest/llms/llama-index/index.html",
-            },
-            {
-              title: "Spark",
-              src: "img/spark.svg",
-              href: "docs/latest/python_api/mlflow.spark.html",
-            },
-            {
-              title: "Keras",
-              src: "img/keras.svg",
-              href: "docs/latest/deep-learning/keras/index.html",
-            },
-            {
-              title: "TensorFlow",
-              src: "img/tensorflow.svg",
-              href: "docs/latest/deep-learning/tensorflow/index.html",
-            },
-            {
-              title: "scikit-learn",
-              src: "img/scikit-learn.svg",
-              href: "docs/latest/models.html#scikit-learn-sklearn",
-            },
-            {
-              title: "XGBoost",
-              src: "img/xgboost.svg",
-              href: "docs/latest/models.html#xgboost-xgboost",
-            },
-            {
-              title: "LightGBM",
-              src: "img/lightgbm.svg",
-              href: "docs/latest/models.html#lightgbm-lightgbm",
-            },
-            {
-              title: "CatBoost",
-              src: "img/catboost.svg",
-              href: "docs/latest/models.html#catboost-catboost",
-            },
-            {
-              title: "Anthropic",
-              src: "img/anthropic.svg",
-              href: "docs/latest/llms/tracing/index.html#automatic-tracing",
-            },
-            {
-              title: "CrewAI",
-              src: "img/crewai.svg",
-              href: "docs/latest/llms/tracing/index.html#automatic-tracing",
-            },
-            {
-              title: "Gemini",
-              src: "img/google-gemini.svg",
-              href: "docs/latest/llms/tracing/index.html#automatic-tracing",
-            },
-            {
-              title: "AutoGen",
-              src: "img/autogen.jpeg",
-              href: "docs/latest/llms/tracing/index.html#automatic-tracing",
-            },
-            {
-              title: "LiteLLM",
-              src: "img/litellm.png",
-              href: "docs/latest/llms/tracing/index.html#automatic-tracing",
-            },
-            {
-              title: "Sentence Transformers",
-              src: "img/sentence-transformers.svg",
-              href: "docs/latest/llms/sentence-transformers/index.html",
-            },
-            {
-              title: "ONNX",
-              src: "img/onnx.svg",
-              href: "docs/latest/models.html#onnx-onnx",
-            },
-            {
-              title: "Spacy",
-              src: "img/spacy.svg",
-              href: "docs/latest/models.html#spacy-spacy",
-            },
-            {
-              title: "FastAI",
-              src: "img/fastai.png",
-              href: "docs/latest/models.html#fastai-fastai",
-            },
-            {
-              title: "StatsModels",
-              src: "img/statsmodels.svg",
-              href: "docs/latest/models.html#statsmodels-statsmodels",
-            },
-            {
-              title: "Prompt flow",
-              src: "img/promptflow.svg",
-              href: "docs/latest/models.html#promptflow-promptflow-experimental",
-            },
-            {
-              title: "JohnSnowLabs",
-              src: "img/johnsnowlab.svg",
-              href: "docs/latest/models.html#john-snow-labs-johnsnowlabs-experimental",
-            },
-            {
-              title: "H2O",
-              src: "img/h2o.svg",
-              href: "docs/latest/models.html#h2o-h2o",
-            },
-            {
-              title: "Prophet",
-              src: "img/prophet.svg",
-              href: "docs/latest/python_api/mlflow.prophet.html",
-            },
-          ]}
-          defaultVisibleCount={windowWidth > MOBILE_LAYOUT_BREAKPOINT ? 16 : 8}
-          renderItem={({ title, src, href }) => (
-            <MiniLogoCard title={title} src={src} href={href} />
+        <BrowserOnly>
+          {() => (
+            <ExpandableGrid
+              items={[
+                {
+                  title: "PyTorch",
+                  src: "img/pytorch.svg",
+                  href: "docs/latest/deep-learning/pytorch/index.html",
+                },
+                {
+                  title: "HuggingFace",
+                  src: "img/huggingface.svg",
+                  href: "docs/latest/llms/transformers/index.html",
+                },
+                {
+                  title: "OpenAI",
+                  src: "img/openai.svg",
+                  href: "docs/latest/llms/openai/index.html",
+                },
+                {
+                  title: "LangChain",
+                  src: "img/langchain.svg",
+                  href: "docs/latest/llms/langchain/index.html",
+                },
+                {
+                  title: "LlamaIndex",
+                  src: "img/llamaindex.svg",
+                  href: "docs/latest/llms/llama-index/index.html",
+                },
+                {
+                  title: "Spark",
+                  src: "img/spark.svg",
+                  href: "docs/latest/python_api/mlflow.spark.html",
+                },
+                {
+                  title: "Keras",
+                  src: "img/keras.svg",
+                  href: "docs/latest/deep-learning/keras/index.html",
+                },
+                {
+                  title: "TensorFlow",
+                  src: "img/tensorflow.svg",
+                  href: "docs/latest/deep-learning/tensorflow/index.html",
+                },
+                {
+                  title: "scikit-learn",
+                  src: "img/scikit-learn.svg",
+                  href: "docs/latest/models.html#scikit-learn-sklearn",
+                },
+                {
+                  title: "XGBoost",
+                  src: "img/xgboost.svg",
+                  href: "docs/latest/models.html#xgboost-xgboost",
+                },
+                {
+                  title: "LightGBM",
+                  src: "img/lightgbm.svg",
+                  href: "docs/latest/models.html#lightgbm-lightgbm",
+                },
+                {
+                  title: "CatBoost",
+                  src: "img/catboost.svg",
+                  href: "docs/latest/models.html#catboost-catboost",
+                },
+                {
+                  title: "Anthropic",
+                  src: "img/anthropic.svg",
+                  href: "docs/latest/llms/tracing/index.html#automatic-tracing",
+                },
+                {
+                  title: "CrewAI",
+                  src: "img/crewai.svg",
+                  href: "docs/latest/llms/tracing/index.html#automatic-tracing",
+                },
+                {
+                  title: "Gemini",
+                  src: "img/google-gemini.svg",
+                  href: "docs/latest/llms/tracing/index.html#automatic-tracing",
+                },
+                {
+                  title: "AutoGen",
+                  src: "img/autogen.jpeg",
+                  href: "docs/latest/llms/tracing/index.html#automatic-tracing",
+                },
+                {
+                  title: "LiteLLM",
+                  src: "img/litellm.png",
+                  href: "docs/latest/llms/tracing/index.html#automatic-tracing",
+                },
+                {
+                  title: "Sentence Transformers",
+                  src: "img/sentence-transformers.svg",
+                  href: "docs/latest/llms/sentence-transformers/index.html",
+                },
+                {
+                  title: "ONNX",
+                  src: "img/onnx.svg",
+                  href: "docs/latest/models.html#onnx-onnx",
+                },
+                {
+                  title: "Spacy",
+                  src: "img/spacy.svg",
+                  href: "docs/latest/models.html#spacy-spacy",
+                },
+                {
+                  title: "FastAI",
+                  src: "img/fastai.png",
+                  href: "docs/latest/models.html#fastai-fastai",
+                },
+                {
+                  title: "StatsModels",
+                  src: "img/statsmodels.svg",
+                  href: "docs/latest/models.html#statsmodels-statsmodels",
+                },
+                {
+                  title: "Prompt flow",
+                  src: "img/promptflow.svg",
+                  href: "docs/latest/models.html#promptflow-promptflow-experimental",
+                },
+                {
+                  title: "JohnSnowLabs",
+                  src: "img/johnsnowlab.svg",
+                  href: "docs/latest/models.html#john-snow-labs-johnsnowlabs-experimental",
+                },
+                {
+                  title: "H2O",
+                  src: "img/h2o.svg",
+                  href: "docs/latest/models.html#h2o-h2o",
+                },
+                {
+                  title: "Prophet",
+                  src: "img/prophet.svg",
+                  href: "docs/latest/python_api/mlflow.prophet.html",
+                },
+              ]}
+              defaultVisibleCount={
+                window.innerWidth > MOBILE_LAYOUT_BREAKPOINT ? 16 : 8
+              }
+              renderItem={({ title, src, href }) => (
+                <MiniLogoCard title={title} src={src} href={href} />
+              )}
+            />
           )}
-        />
+        </BrowserOnly>
 
         <Spacer height={200} />
         <Anchor
