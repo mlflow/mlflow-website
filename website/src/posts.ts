f415fcc6db86792169b568d89b74ddf8cf60dc8c
mlflow-automation <mlflow-automation@users.noreply.github.com>
2024-12-19 16:18:39 +0000

    Add release post
    Signed-off-by: mlflow-automation <mlflow-automation@users.noreply.github.com>

--- website/src/posts.ts
+++ website/src/posts.ts
@@ -350,6 +350,20 @@ export const BLOGS: Blog[] = [
 // Sort by date descending
 export const RELEASES: Release[] = [
   {
+    title: "MLflow 2.19.1",
+    path: "/releases/2.19.1",
+    authors: [
+      {
+        name: "MLflow maintainers",
+        title: "MLflow maintainers",
+        url: "https://github.com/mlflow/mlflow.git",
+        image_url: "https://github.com/mlflow-automation.png",
+      },
+    ],
+    date: "2024-12-19",
+    version: "2.19.1",
+  },
+  {
     title: "MLflow 2.19.0",
     path: "/releases/2.19.0",
     authors: [
