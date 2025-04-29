9563e9fbf08a20884f3ffb6da6d3cf607125ee16
Ademir Beširović <ademir.besirovic@toptal.com>
2025-04-29 16:00:23 +0200

    Format the code

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -2,10 +2,10 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
-  
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">New homepage</h1>
     </div>
-  )
+  );
 }
