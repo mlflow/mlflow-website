6a574cd023230100ae9d587aca5a9ae54ecbbf27
Ademir Beširović <ademir.besirovic1996@gmail.com>
2025-05-06 09:05:00 +0200

    Implement latest new component (#190)

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,14 +1,15 @@
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
-import { SocialWidget } from "../components";
+import { SocialWidget, LatestNews } from "../components";
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
 
   return (
     <div className="flex flex-col items-center justify-center bg-[#0E1416] gap-8 min-h-screen">
-      <div className="w-full px-20 mb-10">
+      <div className="flex flex-col w-full px-20 mb-10 gap-10">
         <SocialWidget variant="red" />
+        <LatestNews variant="red" />
       </div>
     </div>
   );
