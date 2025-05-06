21b98b74c82ba3d3e5983e6d005917c618ec4794
Ademir Beširović <ademir.besirovic@toptal.com>
2025-05-06 13:06:42 +0200

    Implement header component

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,16 +1,18 @@
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
-import { SocialWidget, LatestNews } from "../components";
-import { GetStartedWithMLflow } from "../components";
+import { SocialWidget, LatestNews, Header } from "../components";
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
 
   return (
-    <div className="flex flex-col items-center justify-center bg-[#0E1416] gap-8 min-h-screen">
-      <div className="flex flex-col w-full px-20 mb-10 gap-10">
-        <SocialWidget variant="red" />
-        <LatestNews variant="red" />
+    <div>
+      <div className="flex flex-col items-center justify-center bg-[#0E1416] gap-8 min-h-screen">
+        <Header />
+        <div className="flex flex-col w-full px-20 mb-10 gap-10 mt-20">
+          <SocialWidget variant="red" />
+          <LatestNews variant="red" />
+        </div>
       </div>
     </div>
   );
