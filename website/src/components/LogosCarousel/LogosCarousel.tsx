8bec5612d5dc38333a551d9bfc5e99c6ec5bf10c
Fatih Altinok <fatihaltinok@live.com>
2025-05-20 08:24:33 +0300

    Various fixes (#208)

--- website/src/components/LogosCarousel/LogosCarousel.tsx
+++ website/src/components/LogosCarousel/LogosCarousel.tsx
@@ -17,8 +17,9 @@ export const LogosCarousel = () => {
   return (
     <div className="group relative overflow-hidden whitespace-nowrap py-10 [mask-image:_linear-gradient(to_right,_transparent_0,_white_128px,white_calc(100%-128px),_transparent_100%)] w-full">
       <div className="animate-slide-left-infinite inline-block w-full">
-        {images.map((image) => (
+        {images.map((image, index) => (
           <img
+            key={index}
             className="inline h-16 mx-20 opacity-20"
             src={useBaseUrl(image)}
           />
