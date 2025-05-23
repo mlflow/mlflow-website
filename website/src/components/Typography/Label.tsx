d25e2f50c7dcabf62129413cc5ab84558dc99c47
Fatih Altinok <fatihaltinok@live.com>
2025-05-23 17:18:53 +0300

    Create reusable typography components

---
+++ website/src/components/Typography/Label.tsx
@@ -0,0 +1,18 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import type { PropsWithChildren } from "react";
+
+const styles = cva("leading-[120%] tracking-[8%]", {
+  variants: {
+    size: {
+      l: "font-medium text-[14px]",
+      s: "font-semibold text-[12px]",
+    },
+  },
+});
+
+export const Label = ({
+  size,
+  children,
+}: PropsWithChildren<VariantProps<typeof styles>>) => {
+  return <span className={styles({ size })}>{children}</span>;
+};
