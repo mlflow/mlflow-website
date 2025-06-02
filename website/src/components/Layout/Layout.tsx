b00454d906b9d8c436f3b732a171b3ea5245699b
Fatih Altinok <fatihaltinok@live.com>
2025-06-02 14:46:42 +0300

    Improve glossy cards and the background image
    - Glossy cards have borders and show better color through
    - Background image expands better for more viewports and variants
    - Added flipped versions of the background images for up variant
    - Added radial gradient for too large viewports

--- website/src/components/Layout/Layout.tsx
+++ website/src/components/Layout/Layout.tsx
@@ -6,7 +6,7 @@ import { createContext, PropsWithChildren, useContext } from "react";
 type Props = PropsWithChildren<VariantProps<typeof wrapper>>;
 
 const wrapper = cva(
-  "flex flex-col gap-20 bg-no-repeat w-full pt-42 pb-20 py-20",
+  "flex flex-col gap-20 bg-no-repeat w-full pt-42 pb-20 py-20 max-w-container",
   {
     variants: {
       variant: {
@@ -15,48 +15,58 @@ const wrapper = cva(
         colorful: "",
       },
       direction: {
-        down: "bg-size-[100%_700px] bg-[center_top]",
-        up: "bg-size-[100%_350px] bg-[center_350px]",
+        down: "bg-size-[auto_1000px] bg-[center_top]",
+        up: "bg-size-[auto_800px] bg-[center_top]",
       },
     },
     compoundVariants: [
       {
         variant: "red",
         direction: "down",
-        className:
-          "bg-[linear-gradient(to_bottom,rgba(12,20,20,0),rgba(14,20,20,100)),url('/img/background-image-2.png')]",
+        className: [
+          "bg-[linear-gradient(to_bottom,rgba(12,20,20,0)_0%,rgba(12,20,20,0)_50%,rgba(14,20,20,100)_75%),url('/img/background-image-2.png')]",
+          "2xl:bg-[radial-gradient(farthest-side_at_center_top,rgba(12,20,20,0)_0%,rgba(12,20,20,0)_80%,rgba(14,20,20,100)_97%),url('/img/background-image-2.png')]",
+        ],
       },
       {
         variant: "blue",
         direction: "down",
-        className:
-          "bg-[linear-gradient(to_bottom,rgba(12,20,20,0),rgba(14,20,20,100)),url('/img/background-image-3.png')]",
+        className: [
+          "bg-[linear-gradient(to_bottom,rgba(12,20,20,0)_0%,rgba(12,20,20,0)_50%,rgba(14,20,20,100)_75%),url('/img/background-image-3.png')]",
+          "2xl:bg-[radial-gradient(farthest-side_at_center_top,rgba(12,20,20,0)_0%,rgba(12,20,20,0)_80%,rgba(14,20,20,100)_97%),url('/img/background-image-3.png')]",
+        ],
       },
       {
         variant: "colorful",
         direction: "down",
-        className:
-          "bg-[linear-gradient(to_bottom,rgba(12,20,20,0),rgba(14,20,20,100)),url('/img/background-image-1.png')]",
+        className: [
+          "bg-[linear-gradient(to_bottom,rgba(12,20,20,0)_0%,rgba(12,20,20,0)_50%,rgba(14,20,20,100)_75%),url('/img/background-image-1.png')]",
+          "2xl:bg-[radial-gradient(farthest-side_at_center_top,rgba(12,20,20,0)_0%,rgba(12,20,20,0)_80%,rgba(14,20,20,100)_97%),url('/img/background-image-1.png')]",
+        ],
       },
       {
         variant: "red",
         direction: "up",
         className: [
-          "bg-[linear-gradient(to_top,rgba(12,20,20,0),rgba(14,20,20,100)),url('/img/background-image-2.png')]",
-          "",
+          "bg-[linear-gradient(to_top,rgba(12,20,20,0)_0%,rgba(12,20,20,0)_10%,rgba(14,20,20,100)_40%),url('/img/background-image-2-flipped.png')]",
+          "2xl:bg-[radial-gradient(farthest-side_at_center_bottom,rgba(12,20,20,0)_0%,rgba(12,20,20,0)_10%,rgba(14,20,20,100)_90%),url('/img/background-image-2-flipped.png')]",
         ],
       },
       {
         variant: "blue",
         direction: "up",
-        className:
-          "bg-[linear-gradient(to_top,rgba(12,20,20,0),rgba(14,20,20,100)),url('/img/background-image-3.png')]",
+        className: [
+          "bg-[linear-gradient(to_top,rgba(11,20,20,0)_0%,rgba(12,20,20,0)_10%,rgba(14,20,20,100)_40%),url('/img/background-image-3-flipped.png')]",
+          "2xl:bg-[radial-gradient(farthest-side_at_center_bottom,rgba(12,20,20,0)_0%,rgba(12,20,20,0)_10%,rgba(14,20,20,100)_90%),url('/img/background-image-3-flipped.png')]",
+        ],
       },
       {
         variant: "colorful",
         direction: "up",
-        className:
-          "bg-[linear-gradient(to_top,rgba(12,20,20,0),rgba(14,20,20,100)),url('/img/background-image-1.png')]",
+        className: [
+          "bg-[linear-gradient(to_top,rgba(11,20,20,0)_0%,rgba(12,20,20,0)_10%,rgba(14,20,20,100)_40%),url('/img/background-image-1-flipped.png')]",
+          "2xl:bg-[radial-gradient(farthest-side_at_center_bottom,rgba(12,20,20,0)_0%,rgba(12,20,20,0)_10%,rgba(14,20,20,100)_90%),url('/img/background-image-1-flipped.png')]",
+        ],
       },
     ],
     defaultVariants: {
