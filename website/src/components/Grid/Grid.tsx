2255860c7e493b9e1aeb15c825dc4ae29f2b7cef
Fatih Altinok <fatihaltinok@live.com>
2025-06-03 14:53:50 +0300

    Fix grid filling logic to account for wide grid items

--- website/src/components/Grid/Grid.tsx
+++ website/src/components/Grid/Grid.tsx
@@ -37,17 +37,31 @@ export const Grid = ({
   columns,
 }: VariantProps<typeof gridVariants> &
   PropsWithChildren<{ className?: string }>) => {
-  const numItems = React.Children.count(children);
-  const remainder =
-    typeof columns === "number" ? columns - (numItems % columns) : null;
+  let numSlots = 0;
+  React.Children.forEach(children, (child) => {
+    if (React.isValidElement(child)) {
+      if ("width" in child.props && child.props.width === "wide") {
+        numSlots += 2;
+      } else {
+        numSlots += 1;
+      }
+    }
+  });
+
+  let missingSlots = 0;
+  if (typeof columns === "number") {
+    const rows = Math.ceil(numSlots / columns);
+    const expectedSlots = rows * columns;
+    missingSlots = expectedSlots - numSlots;
+  }
 
   return (
     <div className={gridContainer()}>
       <div className={grid({ columns, className })}>
         {children}
-        {remainder
-          ? Array.from({ length: remainder }, (_, i) => <GridItem key={i} />)
-          : null}
+        {Array.from({ length: missingSlots }, (_, i) => (
+          <GridItem key={i} />
+        ))}
       </div>
     </div>
   );
