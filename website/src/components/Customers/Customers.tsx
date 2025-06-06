260393cb6b3835962b233251d42a7d9cfa690cfe
xq-yin <xiaoqian.yin@databricks.com>
2025-06-06 16:57:11 -0700

    homepage changes

--- website/src/components/Customers/Customers.tsx
+++ website/src/components/Customers/Customers.tsx
@@ -3,8 +3,7 @@ import { LogosCarousel, Section } from "..";
 export const Customers = () => {
   return (
     <Section
-      label="Customers"
-      title="Trusted by thousands of businesses and research teams"
+      title="Trusted by thousands of organizations and research teams"
     >
       <LogosCarousel />
     </Section>
--- website/src/constants.ts
+++ website/src/constants.ts
@@ -1,10 +1,4 @@
 export const MOBILE_LAYOUT_BREAKPOINT = 996;
 export const MLFLOW_DOCS_URL = "https://mlflow.org/docs/latest/";
-export const MLFLOW_LOGIN_URL =
-  "https://login.databricks.com/?destination_url=/ml/experiments&dbx_source=MLFLOW_WEBSITE&source=MLFLOW_WEBSITE&provider=MLFLOW";
-export const MLFLOW_SIGNUP_URL =
-  "https://signup.databricks.com/?destination_url=/ml/experiments-signup?source=TRY_MLFLOW&dbx_source=TRY_MLFLOW&signup_experience_step=EXPRESS&provider=MLFLOW";
-export const MLFLOW_GET_STARTED_URL =
-  "https://mlflow.org/docs/latest/getting-started/";
-export const MLFLOW_GENAI_GET_STARTED_URL =
-  "https://login.databricks.com/signup?destination_url=/ml/experiments-signup?source=TRY_MLFLOW&dbx_source=TRY_MLFLOW&signup_experience_step=EXPRESS&provider=MLFLOW";
+export const MLFLOW_DBX_TRIAL_URL =
+  "https://signup.databricks.com/?destination_url=/ml/experiments-signup?source=MLFLOW_ORG&dbx_source=TRY_MLFLOW&signup_experience_step=EXPRESS&provider=MLFLOW";
