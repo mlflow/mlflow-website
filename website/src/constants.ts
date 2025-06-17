674a7798bfddfb626866bd76a7682132be9a8147
Cathy Yin <xiaoqian.yin@databricks.com>
2025-06-17 02:35:12 -0700

    Update utm_source link for DBX signup flow (#310)

--- website/src/constants.ts
+++ website/src/constants.ts
@@ -1,6 +1,6 @@
 export const MOBILE_LAYOUT_BREAKPOINT = 996;
 export const MLFLOW_DOCS_URL = "https://mlflow.org/docs/latest/";
 export const MLFLOW_DBX_TRIAL_URL =
-  "https://signup.databricks.com/?destination_url=/ml/experiments-signup?source=MLFLOW_ORG&dbx_source=TRY_MLFLOW&signup_experience_step=EXPRESS&provider=MLFLOW";
+  "https://signup.databricks.com/?destination_url=/ml/experiments-signup?source=TRY_MLFLOW&dbx_source=TRY_MLFLOW&signup_experience_step=EXPRESS&provider=MLFLOW&utm_source=mlflow_org";
 export const MLFLOW_DBX_INSTALL_URL =
   "https://docs.databricks.com/aws/en/mlflow/mlflow-3-install";
