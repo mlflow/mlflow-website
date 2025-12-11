b15167a9d314d3598bac3b573b8516f4f1f7d367
Kevin Lin <kevin.lin@databricks.com>
2025-12-11 09:16:42 +0800

    Revert change

--- website/src/pages/genai/governance.tsx
+++ website/src/pages/genai/governance.tsx
@@ -1,3 +1,4 @@
+import { MLFLOW_DOCS_URL } from "@site/src/constants";
 import {
   Layout,
   AboveTheFold,
@@ -5,7 +6,6 @@ import {
   HeroImage,
   StickyGrid,
 } from "../../components";
-import { MLFLOW_DOCS_URL } from "@site/src/constants";
 import CardHero from "@site/static/img/GenAI_governance/GenAI_governance_hero.png";
 import Card1 from "@site/static/img/GenAI_governance/GenAI_governance_1.png";
 import Card2 from "@site/static/img/GenAI_governance/GenAI_governance_2.png";
