0d878f4102cd1f50648f26b259d33e0a585929e7
B-Step62 <yuki.watanabe@databricks.com>
2026-02-02 09:21:50 +0900

    fix mobile view
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- website/src/theme/ReleaseLayout/index.js
+++ website/src/theme/ReleaseLayout/index.js
@@ -1,23 +1,29 @@
 import React from "react";
 import ThemeLayout from "@theme/Layout";
+import { useWindowSize } from "@docusaurus/theme-common";
 
 import { Layout } from "../../components";
 import BlogSidebar from "@theme/BlogSidebar";
 
 export default function ReleaseLayout(props) {
   const { sidebar, toc, children, ...layoutProps } = props;
+  const windowSize = useWindowSize();
+  const isDesktop = windowSize === "desktop";
+
   return (
     <ThemeLayout {...layoutProps}>
       <Layout>
         <div className="max-w-7xl mx-auto">
-          <div className="flex flex-row gap-12">
-            <div className="contents text-white **:[nav_ul]:!mb-6">
-              <BlogSidebar sidebar={sidebar} />
-            </div>
+          <div className="flex flex-col md:flex-row gap-6 md:gap-12">
+            {isDesktop && (
+              <div className="contents text-white **:[nav_ul]:!mb-6">
+                <BlogSidebar sidebar={sidebar} />
+              </div>
+            )}
             <main
               itemScope
               itemType="https://schema.org/Blog"
-              className="hyphens-auto break-word"
+              className="hyphens-auto break-word overflow-x-hidden w-full"
             >
               {children}
             </main>
