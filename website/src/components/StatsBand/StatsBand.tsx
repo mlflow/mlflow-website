0e84f0e7cae02928bce9bf88bd99c620a4827ac9
Corey Zumar <39497902+dbczumar@users.noreply.github.com>
2026-02-16 16:59:51 -0800

    Use AI Platform to cover MLOps and Agents (#470)

--- website/src/components/StatsBand/StatsBand.tsx
+++ website/src/components/StatsBand/StatsBand.tsx
@@ -16,7 +16,7 @@ const GitHubIcon = (props: SVGProps<SVGSVGElement>) => (
 export const StatsBand = () => {
   return (
     <Section
-      title="Most Adopted Open-Source MLOps Platform"
+      title="Most Adopted Open-Source AI Platform"
       body="Backed by Linux Foundation, MLflow has been fully committed to open-source for 5+ years. Now trusted by thousands of organizations and research teams worldwide."
       align="center"
     >
