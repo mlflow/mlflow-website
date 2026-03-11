2f9760c21e1ffee6b6bed67226b91c6beb0dc77b
Puneet Jain <puneet.jain159@gmail.com>
2026-03-11 00:05:46 +0000

    Refactor code formatting in Arize Phoenix alternative page
    - Improved readability by adjusting the formatting of constant declarations.
    - Added a blank line for better separation of code sections.
    
    Signed-off-by: Puneet Jain <puneet.jain159@gmail.com>

--- website/src/pages/arize-phoenix-alternative.tsx
+++ website/src/pages/arize-phoenix-alternative.tsx
@@ -7,9 +7,12 @@ import { MLFLOW_DOCS_URL, MLFLOW_GENAI_DOCS_URL } from "@site/src/constants";
 import { CopyButton } from "../components/CodeSnippet/CopyButton";
 import { customNightOwl, CODE_BG } from "../components/CodeSnippet/codeTheme";
 
-const MLFLOW_UI_SRC = "/img/arize-phoenix-alternative/mlflow-ui.png";
-const PHOENIX_UI_SRC = "/img/arize-phoenix-alternative/arize-phoenix-ui.png";
-const MLFLOW_LOGO_SRC = "/img/arize-phoenix-alternative/mlflow-logo.png";
+const MLFLOW_UI_SRC =
+  "/img/arize-phoenix-alternative/mlflow-ui.png";
+const PHOENIX_UI_SRC =
+  "/img/arize-phoenix-alternative/arize-phoenix-ui.png";
+const MLFLOW_LOGO_SRC =
+  "/img/arize-phoenix-alternative/mlflow-logo.png";
 const PHOENIX_LOGO_SRC =
   "/img/arize-phoenix-alternative/arize-phoenix-logo-placeholder.svg";
 const SEO_TITLE =
@@ -1201,6 +1204,7 @@ export default function ArizePhoenixAlternative() {
             </ul>
           </div>
         </div>
+
         <aside className="article-sidebar">
           <p className="toc-title">ON THIS PAGE</p>
           <ul>
