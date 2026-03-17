6add9b98eabb5364111533c3490b9559df462ea0
B-Step62 <yuki.watanabe@databricks.com>
2026-03-18 02:28:45 +0900

    Add MLflow + OpenHands blog
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

---
+++ website/src/components/BlogImage/BlogImage.tsx
@@ -0,0 +1,21 @@
+import { CSSProperties } from "react";
+
+type Props = {
+  src: string;
+  alt: string;
+  width?: string;
+  center?: boolean;
+  style?: CSSProperties;
+};
+
+export function BlogImage({ src, alt, width = "100%", center = false, style }: Props) {
+  return (
+    <img
+      src={src}
+      alt={alt}
+      width={width}
+      className={`rounded-lg shadow-md mb-4${center ? " mx-auto block" : ""}`}
+      style={style}
+    />
+  );
+}
