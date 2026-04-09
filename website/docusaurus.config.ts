a65580272cf6077c1e50367e4cde1b633a03ec94
kevin-lyn <kevin.lin@databricks.com>
2026-04-09 10:06:08 +0800

    Fix canonical url scheme
    Signed-off-by: kevin-lyn <kevin.lin@databricks.com>

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   favicon: "img/mlflow-favicon.ico",
 
   // Set the production url of your site here
-  url: "http://mlflow.org",
+  url: "https://mlflow.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl,
